c4d09232d1358269b261d18131662e36edfcb66f
c1nd3r3114 <hunghainhi2003@gmail.com>
2025-01-16 15:15:26 +0700

    login to dockerhub

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,11 @@ set -e
 IMAGE_NAME=$IMAGE_NAME
 DOCKER_REGISTRY=$DOCKER_REGISTRY
 
+pull_image() {
+  echo "Pulling Docker image..."
+  sudo docker pull $DOCKER_REGISTRY/$IMAGE_NAME
+}
+
 deploy_image() {
   echo "Deploying Docker image..."
   sudo docker run -d -p 3000:3000 $DOCKER_REGISTRY/$IMAGE_NAME
