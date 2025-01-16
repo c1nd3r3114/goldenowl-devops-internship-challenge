7b541706687c07c09a58aaf1f4256b96b233cd32
c1nd3r3114 <hunghainhi2003@gmail.com>
2025-01-16 14:50:18 +0700

    deploy image into ec2

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+IMAGE_NAME=$IMAGE_NAME
+DOCKER_REGISTRY=$DOCKER_REGISTRY
+
+deploy_image() {
+  echo "Deploying Docker image..."
+  sudo docker run -d -p 3000:3000 $DOCKER_REGISTRY/$IMAGE_NAME
+}
+
+deploy_image
