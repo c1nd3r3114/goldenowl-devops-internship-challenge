b3abade2895e76a39ea7f31c06007dccefe15511
c1nd3r3114 <hunghainhi2003@gmail.com>
2025-01-16 14:56:35 +0700

    add latest tag

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ DOCKER_REGISTRY=$DOCKER_REGISTRY
 
 deploy_image() {
   echo "Deploying Docker image..."
-  sudo docker run -d -p 3000:3000 $DOCKER_REGISTRY/$IMAGE_NAME
+  sudo docker run -d -p 3000:3000 $DOCKER_REGISTRY/$IMAGE_NAME:latest
 }
 
 deploy_image
