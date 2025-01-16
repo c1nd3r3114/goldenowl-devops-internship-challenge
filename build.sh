317f5e184c5f4bd1fa7a479dafd79894b8d904c6
c1nd3r3114 <hunghainhi2003@gmail.com>
2025-01-16 14:25:16 +0700

    remove image tag

--- build.sh
+++ build.sh
@@ -9,12 +9,12 @@ DOCKER_REGISTRY=$DOCKER_REGISTRY
 
 build_image() {
   echo "Building Docker image..."
-  docker build -t $DOCKER_REGISTRY/$IMAGE_NAME:$CIRCLE_SHA1 .
+  docker build -t $DOCKER_REGISTRY/$IMAGE_NAME .
 }
 
 push_image() {
   echo "Pushing Docker image..."
-  docker push $DOCKER_REGISTRY/$IMAGE_NAME:$CIRCLE_SHA1
+  docker push $DOCKER_REGISTRY/$IMAGE_NAME
 }
 
 build_image $IMAGE_NAME 
